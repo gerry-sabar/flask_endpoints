c5feef5e4a4335f90f9328370500523c83e6cb7e
Gerry <gerrysabar@gmail.com>
2020-01-17 01:17:32 +0700

    documentations

---
+++ activate.sh
@@ -0,0 +1,8 @@
+export FLASK_APP=app
+
+#you can remove this in deployment server
+#it's useful so we can see any change from our code directly when developing
+export FLASK_DEBUG=1
+
+#running the app
+flask run
--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_jwt_extended import (
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/flask-restplus'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/endpoints'
 CORS(app)
 db = SQLAlchemy(app)
 db.init_app(app)
@@ -38,3 +38,6 @@ def seed():
     fake = Faker()
     for x in range(3):
         UserApi.seed(fake)
+
+if __name__ == "__main__":
+  app.run(host='127.0.0.1', port=5000)
